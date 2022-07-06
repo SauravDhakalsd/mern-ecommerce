a22326b59cb38139f1f93ad8b2b327e5b34457f5
Saurav Dhakal <86107331+SauravDhakalsd@users.noreply.github.com>
2022-07-06 17:51:19 +0545

    Add files via upload

---
+++ client/build/precache-manifest.05628959a9a166ed5031e4bb43cb83d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd594341064f1c186d6353f7f04848b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79980bc37781a15fe05c",
+    "url": "/static/css/main.2c31312f.chunk.css"
+  },
+  {
+    "revision": "20ffd94122ca229bb574",
+    "url": "/static/js/2.5afdb0a9.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.5afdb0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79980bc37781a15fe05c",
+    "url": "/static/js/main.504a8888.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.504a8888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
